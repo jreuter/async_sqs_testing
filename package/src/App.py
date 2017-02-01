a35140663629a92a778d81ee4dc0b094ab526533
Andrew Aslinger <aaslinger@openwhere.com>
2017-02-01 12:10:55 -0500

    Fix and read message

--- package/src/App.py
+++ package/src/App.py
@@ -1,46 +1,52 @@
-import aiobotocore
+import sys
 import asyncio
+import aiobotocore
 import logging
 from botocore.exceptions import ClientError
 from random import randint
 
+logging.basicConfig( stream=sys.stdout, level=logging.INFO)
+
+
 async def coro1():
     res = randint(0, 7)
     await asyncio.sleep(res)
-    logging.error('coro1 finished with output {}'.format(res))
+    logging.info('coro1 finished with output {}'.format(res))
     return res
 
 async def coro2():
     res = await coro1()
     res = res * res
     await asyncio.sleep(res)
-    logging.error('coro2 finished with output {}'.format(res))
+    logging.info('coro2 finished with output {}'.format(res))
     return res
 
 async def coro3(loop):
-    queue_name = 'jreuter-edge-detection'
-    logging.error('About to call SQS')
+    queue_name = 'aaslinger-edge-detection'
+    logging.info('C3 - About to call SQS')
     session = aiobotocore.get_session(loop=loop)
-    client = session.create_client('sqs', region_name='us-east-1')
-    try:
-        queue = await client.get_queue_url(QueueName=queue_name)
-    except ClientError as e:
-        if e.response['Error']['Code'] == "AWS.SimpleQueueService.NonExistentQueue":
-            logging.info(e.response['Error']['Message'])
-            logging.info("Creating SQS queue %s in region %s...", queue_name, 'us-east-1')
-            queue = await client.create_queue(QueueName=queue_name)
+    async with session.create_client('sqs', region_name='us-east-1') as client:
+        try:
+            queue = await client.get_queue_url(QueueName=queue_name)
+        except ClientError as e:
+            if e.response['Error']['Code'] == "AWS.SimpleQueueService.NonExistentQueue":
+                logging.info(e.response['Error']['Message'])
+                logging.info("Creating SQS queue %s in region %s...", queue_name, 'us-east-1')
+                queue = await client.create_queue(QueueName=queue_name)
+            else:
+                raise
+        queue_url = queue.get('QueueUrl')
+        logging.info("C3 - recieving messages")
+        result = await client.receive_message(WaitTimeSeconds=20,
+                                              MaxNumberOfMessages=1,
+                                              QueueUrl=queue_url)
+
+        if 'Messages' in result:
+            logging.info('C3 - We got some messages')
+            for message in result['Messages']:
+                logging.info(message['Body'])
         else:
-            raise
-    queue_url = queue.get('QueueUrl')
-
-    result = await client.receive_message(WaitTimeSeconds=20,
-                                          MaxNumberOfMessages=1,
-                                          QueueUrl=queue_url)
-
-    if 'Messages' in result:
-        logging.info('We got some messages')
-    else:
-        logging.info('We got no messages')
+            logging.info('C3 - We got no messages')
     return
 
 async def main(loop):
